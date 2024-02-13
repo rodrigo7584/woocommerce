b84aa2d3809f853c2c80013e615839cdbbbc9e3e
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-02-12 21:55:11 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cojiba</title>
+</head>
+<body>
+  <?php
+    echo "hello world s";
+    ?>
+</body>
+</html>
