a9a527603d2cc9df9c0afbae1568cf3dc368b3b4
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-02-13 14:42:34 -0300

    primeiro loop

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cojiba</title>
 </head>
+
 <body>
-  <?php
-    echo "hello world s";
-    ?>
+  <?php if(have_posts( )){ while(have_posts( )){ the_post(); ?>
+  <p>tem pagina</p>
+  <?php }} else { ?>
+  <p>Página não encontrada</p>
+  <?php }; ?>
 </body>
+
 </html>
