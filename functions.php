2838dadf21a38951dd9f16bd9cca9e65b3442ac1
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-02-14 18:10:13 -0300

    14/02/2024

--- functions.php
+++ functions.php
@@ -1,12 +1,25 @@
 <?php
-
+/*FUNÇÃO QUE DIZ AO WORDPRESS QUE O TEMA OFERECE SUPORTE AO WOOCOMMERCE */
 function cojiba_add_woocommerce_support(){
   add_theme_support('woocommerce');
 };
 add_action('after_setup_theme', 'cojiba_add_woocommerce_support');
 
+/*FUNÇÃO QUE ADICIONA O ARQUIVO CSS AO TEMA*/
 function cojiba_css(){
   wp_register_style('cojiba-style', get_template_directory_uri() . '/style.css', [] ,'1.0.0', false);
   wp_enqueue_style('cojiba-style');
 };
 add_action('wp_enqueue_scripts', 'cojiba_css');
+
+/*FUNÇÃO PARA HABILITAR SUPORTE A LOGO CUSTOMIZADA*/
+function cojiba_theme_suports() {
+  add_theme_support('custom-logo', array(
+      'height'      => 45, // Altura máxima do logotipo
+      'width'       => 185, // Largura máxima do logotipo
+      'flex-height' => true,
+      'flex-width'  => true,
+      'unlink-homepage-logo' => false, 
+  ));
+}
+add_action('after_setup_theme', 'cojiba_theme_suports');
--- header.php
+++ header.php
@@ -4,8 +4,35 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name')?> | <?php the_title();?></title>
+  <title><?php bloginfo('name')?><?php wp_title('|');?></title>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class();?>>
+  <!-- BLOCO DE CODIGO QUE BUSCA UMA CUSTOM LOGO 
+  SE NÃO ENCONTRA ELE COLOCA O NOME DO PRODUTO EM UMA VARIAVEL -->
+  <?php
+  $custom_logo = get_theme_mod('custom_logo');
+
+  $logo = wp_get_attachment_image_src($custom_logo, 'full');
+
+  if (has_custom_logo()) {
+      $logo_image ='<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+  } else {
+      $logo_image ='<h1>' . get_bloginfo('name') . '</h1>';
+  }
+  ?>
+  <?php 
+    `<form action='`bloginfo('url')`/loja/' method='get'>
+    </form>` 
+  ?>
+
+
+  <header class="header">
+    <a href="/">
+      <?php echo $logo_image; ?>
+    </a>
+    <div class="search">
+      <?php get_product_search_form();?>
+    </div>
+  </header>
